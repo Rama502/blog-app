9470c63d2d1a8dbf97f176746b84c64588e4793c
Rama502 <21P31A0502@acet.ac.in>
2025-04-02 23:07:24 +0530

    build

---
+++ src/pages/CreateBlog.js
@@ -0,0 +1,11 @@
+function CreateBlog() {
+    return (
+      <div>
+        <h2>Create a New Blog</h2>
+        <p>Blog creation form will be here.</p>
+      </div>
+    );
+  }
+  
+  export default CreateBlog;
+  
---
+++ src/pages/Signup.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Signup() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8000/api/signup", formData);
+      alert("Signup Successful!");
+    } catch (error) {
+      alert("Signup Failed!");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
